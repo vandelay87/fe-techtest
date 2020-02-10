07bb986b42b1a3e30aaf00890b70268e737e2868
vandelay87 <akliaissat@outlook.com>
2020-02-10 21:56:37 +0000

    display numbers and add state management

--- src/multiplicationTest.js
+++ src/multiplicationTest.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const MultiplicationTest = ({ limit }) => (<p>limit: {limit}</p>);
+const MultiplicationTest = ({ limit }) => {
+  const [numberList, setNumberList] = useState(
+    Array(limit).fill().map((item, index) => (
+      {
+        key: index + 1,
+        value: index + 1,
+        highlight: false,
+      }
+    ))
+  );
+
+  const highlightNumbers = (numberVal) => (
+    setNumberList(numberList.map((item, index) => (
+      {
+        key: index + 1,
+        value: index + 1,
+        highlight: (index + 1) % numberVal === 0 ? true : false
+      }
+    )))
+  );
+
+  return (
+    <div>
+      {numberList.map((item) => (
+        <button onClick={() => highlightNumbers(item.value)} key={item.key} className='item'>{item.value}</button>
+      ))}
+    </div>
+  )
+};
 
 MultiplicationTest.propTypes = {
   limit: PropTypes.number,
