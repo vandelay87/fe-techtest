d2fc1307acc41e4e1332b14586f5961d307be018
vandelay87 <akliaissat@outlook.com>
2020-02-11 08:24:32 +0000

    remove css dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
       }
     ],
   },
